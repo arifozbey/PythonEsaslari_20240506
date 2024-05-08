169cc7ea221e3c182718b60724e7a677160abebe
Abdullah Kise <abdullahkise@hotmail.com>
2024-05-08 16:02:59 +0300

    gun3

---
+++ Gun3/kutuphanem/nesnelerim.py
@@ -0,0 +1,8 @@
+#yukarıdaki komut bu hücre içeriğini belirtilen yoldaki dosyaya yazdı.
+#dosya yolu değişkenden gelecekse $ işareti ile değişken yazılır.
+def c2f(c):
+    return c * 1.8 + 32
+
+#---------
+yazar = "Abdullah Kise"
+mail = "abdullahkise@hotmail.com"
